e096d0a3cd588fa6224c751b96d9f618a0660f74
jade-victor <jadevictor247@gmail.com>
2024-10-07 23:03:29 +0100

    Day 15 updated.The main coffee_machine() function ties it all together

--- WEEKk 2/Day 15/1_coffe_machine.py
+++ WEEKk 2/Day 15/1_coffe_machine.py
@@ -5,6 +5,7 @@ from coffe_machine_menu import resources, MENU
 def resource_sufficient(menu_ingredient):
     #3 hot flavours (latte/espresso/cappuccino)
     # 2. check resources sufficient
+    """Ensures enough ingredients for the selected coffee."""
     for item in menu_ingredient:
         if menu_ingredient[item] >= resources[item]:
             print(f"Sorry there is not enough {item}")
@@ -15,17 +16,17 @@ def process_coins():
     # it has 4types of coin the penny(1 cent:N0.01),
     # nickel(5 cents:N0.05) ,dime(10 cents:N0.10) and quarter(25 cents:N0.25)
     # 3 process coins
-    """Returns the calculated coins"""
+    """Handles calculating the total value of coins inserted."""
     print("Insert coins.")
     total = int(input("how many quarters? ")) * 0.25
     total += int(input("how many dime? ")) * 0.10
     total += int(input("how many nickel? ")) * 0.05
     total += int(input("how many penny? ")) * 0.01
     return  total
-
+ profit = 0
 def transaction_successful(money_received, flavour_cost):
     # 4 check transaction successful
-    """Return True when the transaction is accepted or False transaction insufficient"""
+    """Determines if the user has inserted enough money and returns either True or False."""
     if money_received >= flavour_cost:
         change = round(money_received - flavour_cost, 2)
         print(f"Here is your N{change} change")
@@ -38,32 +39,32 @@ def transaction_successful(money_received, flavour_cost):
 
 def make_coffee(flavour_name, ingredients):
     # 5 make coffee
+    """Prepares the coffee if the transaction is successful."""
     for item in ingredients:
         resources[item] -= ingredients[item]
     print(f"Here is your {flavour_name}☕🥛. Enjoy")
 
-profit = 0
-is_off = True
-while is_off:
-    user = input("what would you like? (latte/espresso/cappuccino): ").lower()
-    # # 2
-    # TODO: Turn off the machine by entering "off" to the prompt. (my code should execute when this happens)
-    if user == "off":
-        is_off = False
-    # 1 print report
-    elif user == "report":
-        print(f"""
-        water: {resources['water']}ml,
-        milk: {resources['milk']}ml,
-        coffee: {resources['coffee']}g,
-        profit: N{profit}
-        """)
-    else:
-        flavour = MENU[user]
-        if resource_sufficient(flavour['ingredients']):
-            transaction = process_coins()
-            transaction_successful(money_received=transaction, flavour_cost=flavour['cost'])
-            make_coffee(flavour_name=user, ingredients=flavour['ingredients'])
-
-
-
+def coffee_machine():
+    """Integrates all the logic together for smooth operation."""
+    is_off = True
+    while is_off:
+        user = input("what would you like? (latte/espresso/cappuccino): ").lower()
+        # # 2
+        # TODO: Turn off the machine by entering "off" to the prompt. (my code should execute when this happens)
+        if user == "off":
+            is_off = False
+        # 1 print report
+        elif user == "report":
+            print(f"""
+            water: {resources['water']}ml,
+            milk: {resources['milk']}ml,
+            coffee: {resources['coffee']}g,
+            profit: N{profit}
+            """)
+        else:
+            flavour = MENU[user]
+            if resource_sufficient(flavour['ingredients']):
+                transaction = process_coins()
+                transaction_successful(money_received=transaction, flavour_cost=flavour['cost'])
+                make_coffee(flavour_name=user, ingredients=flavour['ingredients'])
+coffee_machine()
